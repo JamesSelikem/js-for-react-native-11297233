9a97f5b88b02c6cc3bb25e4c6460b9a35d788567
JamesSelikem <jamesselikem1@gmail.com>
2024-05-17 21:22:16 +0000

    creation of function formatArrayStrings

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -16,3 +16,24 @@ function processArray(array) {
 var inputArray = [1,2,3,4,5,6,7,8,9,10];
 var outputArray = processArray(inputArray);
 console.log(outputArray);
+
+
+function formatArrayStrings(strings, numbers) {
+    var formattedStrings = [];
+    
+    for (var i = 0; i < strings.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            formattedStrings.push(strings[i].toUpperCase());
+        } else {
+            formattedStrings.push(strings[i].toLowerCase());
+        }
+    }
+    
+    return formattedStrings;
+}
+
+var inputStrings = ["First", "Assignment", "Mobile", "Development"];
+var inputNumbers = [1, 2, 3, 4, 5];
+var processedNumbers = processArray(inputNumbers);
+var formattedStrings = formatArrayStrings(inputStrings, processedNumbers);
+console.log(formattedStrings);
